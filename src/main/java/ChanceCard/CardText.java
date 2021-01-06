a847e45284a46881a22266176c7961ce30c63cfb
chen <s205420@student.dtu.dk>
2021-01-06 16:44:11 +0100

    cardpile

--- src/main/java/ChanceCard/CardText.java
+++ src/main/java/ChanceCard/CardText.java
@@ -1,9 +1,22 @@
 package ChanceCard;
 
+import org.w3c.dom.Text;
+
 public class CardText extends ChanceCard {
     ChanceCard[] CardPile;
     String T;
     int ID;
     int TopCard;
+
+
+    public ChanceCard() {
+        CardPile = new ChanceCard[35];
+        int i = 0;
+        CardPile[i] = new IncreasePrice()
+        CardPile[i++] = new ReceiveMoney("Modtag 500 kr.", i, 500);
+        }
+}
+
+
 
 }
