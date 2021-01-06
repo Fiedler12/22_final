8f146d6d8f8ed505a46b30eb989d80409afac7a7
chen <s205420@student.dtu.dk>
2021-01-06 20:31:37 +0100

    cardpile 1.1

--- src/main/java/ChanceCard/CardText.java
+++ src/main/java/ChanceCard/CardText.java
@@ -1,6 +1,5 @@
 package ChanceCard;
 
-import org.w3c.dom.Text;
 
 public class CardText extends ChanceCard {
     ChanceCard[] CardPile;
@@ -12,8 +11,34 @@ public class CardText extends ChanceCard {
     public ChanceCard() {
         CardPile = new ChanceCard[35];
         int i = 0;
-        CardPile[i] = new IncreasePrice()
-        CardPile[i++] = new ReceiveMoney("Modtag 500 kr.", i, 500);
+        CardPile[i] = new IncreasePrice("Oliepriserne er steget, og de skal betale 500 kr pr hus og 2000kr pr hotel", i, 500,2000);
+        CardPile[i++] = new IncreasePrice("Ejendomsskatten er steget. Ekstraudgifterne er 800kr pr hus, 2300kr pr hotel",i++,800,2300);
+        CardPile[i++] = new PayMoney("De har kørt frem for fuldt stop, betal 1000 kr i bøde",i++,1000);
+        CardPile[i++] = new PayMoney("Betal for vognvask og smøring 300kr", i++,300);
+        CardPile[i++] = new PayMoney("Betal 200 kr for levering af 2 kasser øl",i++,200);
+        CardPile[i++] = new PayMoney("Betal 3000 for reparation af deres vogn",i++,3000);
+        CardPile[i++] = new PayMoney("Betal 3000 for reparation af deres vogn",i++,3000);
+        CardPile[i++] = new PayMoney("De har købt 4 nye dæk til deres vogn",i++,1000);
+        CardPile[i++] = new PayMoney("De har fået en parkeringskøbe, betal 200 kr i bøde",i++,200 );
+        CardPile[i++] = new PayMoney("Betal deres bilforsikring",i++,1000);
+        CardPile[i++] = new PayMoney("De har været udenlands og købt for manger smønger, betal 200kr i told",i++,200);
+        CardPile[i++] = new PayMoney("Tandlægeregning betal 2000kr",i++,2000);
+        CardPile[i++] = new ReceiveMoney("De har vundet i klasselotteriet. Modtag 500kr",i++,500);
+        CardPile[i++] = new ReceiveMoney("De har vundet i klasselotteriet. Modtag 500kr",i++,500);
+        CardPile[i++] = new ReceiveMoney("De modtager deres aktieudbytte. Motag 1000 kr af banken",i++,1000);
+        CardPile[i++] = new ReceiveMoney("De modtager deres aktieudbytte. Motag 1000 kr af banken",i++,1000);
+        CardPile[i++] = new ReceiveMoney("De modtager deres aktieudbytte. Motag 1000 kr af banken",i++,1000);
+        CardPile[i++] = new ReceiveMoney("Kommunen har eftergivet et kvartals skat. Hæv i banken 3000kr",i++,3000);
+        CardPile[i++] = new ReceiveMoney("De havde en række med elleve rigtige i tipning modtag 1000kr",i++,1000);
+        CardPile[i++] = new ReceiveMoney("Grundet dyrtiden har de fået gageforhøjelse, modtag 1000kr",i++,1000);
+        CardPile[i++] = new ReceiveMoney("Deres præmieobligation er udtrykket, de modtager 1000kr",i++,1000);
+        CardPile[i++] = new ReceiveMoney("Deres præmieobligation er udtrykket, de modtager 1000kr",i++,1000);
+        CardPile[i++] = new ReceiveMoney("De har solgt nogle gamle møbler på auktion, modtag 1000kr",i++,1000);
+        CardPile[i++] = new ReceiveMoney("Værdien af egen avl fra nyttehaven udgør 200kr som de modtager",i++,200);
+        CardPile[i++] = new ReceiveMoney("De modtager Matador-legatet på 40000kr, men kun hvis dine værdier ikke overstiger 15000",i++,40000);
+        CardPile[i++] = new MoneyFromPlayer("Det er deres fødselsdag, motag 200 kr fra hver medspiller",i++,200);
+        CardPile[i++] = new MoneyFromPlayer("De har lagt penge ud til et sammenskudsgilde.Mærkværdigvis betaler alle straks, modtag 500kr fra hver medspiller",i++,500);
+        CardPile[i++] = new MoneyFromPlayer("De skal holde familiefest og får et tilskud fra hver medspiller på 500kr",i++,500);
         }
 }
 
