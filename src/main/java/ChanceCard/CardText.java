50d6ce7dfc291fa4ab3a1febf457cc5536cf59cd
Alex <s201172@student.dtu.dk>
2021-01-06 21:03:51 +0100

    chanccard 1.2

--- src/main/java/ChanceCard/CardText.java
+++ src/main/java/ChanceCard/CardText.java
@@ -39,8 +39,25 @@ public class CardText extends ChanceCard {
         CardPile[i++] = new MoneyFromPlayer("Det er deres fødselsdag, motag 200 kr fra hver medspiller",i++,200);
         CardPile[i++] = new MoneyFromPlayer("De har lagt penge ud til et sammenskudsgilde.Mærkværdigvis betaler alle straks, modtag 500kr fra hver medspiller",i++,500);
         CardPile[i++] = new MoneyFromPlayer("De skal holde familiefest og får et tilskud fra hver medspiller på 500kr",i++,500);
-        }
-}
+        CardPile[i++] = new Move("Ryk frem til START", i++,39 );//placement for start når vi engang for lavet brattet
+        CardPile[i++] = new Move("Ryk frem til START", i++,39 );//placement for start når vi engang for lavet brattet
+        CardPile[i++] = new Move("Ryk tre felter frem",i++,+3);
+        CardPile[i++] = new Move("Ryk tre felter tilbage",i++,-3);
+        CardPile[i++] = new Move("Ryk tre felter tilbage",i++,-3);
+        CardPile[i++] = new Move("Ryk frem til Frederiksberg Alle. Hvis De passere START, indkasser da 4000kr.",i++,38);
+        CardPile[i++] = new Move("Ryk frem til det nærmeste rederi og betal ejeren to gange den leje han ellers er berettiget til, hvis selskabet ikke ejes af nogen kan De købe det af banken",i++,shipping;
+        CardPile[i++] = new Move("Ryk frem til det nærmeste rederi og betal ejeren to gange den leje han ellers er berettiget til, hvis selskabet ikke ejes af nogen kan De købe det af banken",i++,shipping;
+        CardPile[i++] = new Move("Tag Mols-Linien, flyt brikken frem og hvis De passerer START indkasser da 4000kr",i++,25);
+        CardPile[i++] = new Move("Ryk frem til Grønningen, hvis De passere start indkasser da 4000kr.",i++,29);
+        CardPile[i++] = new Move("Ryk frem til Vimmelskaftet, hvis de passerer start indkasser da 4000kr.",i++,20);
+        CardPile[i++] = new Move("Tag med den nærmeste færge, hvis de passerer start indkasser da 4000kr.",i++,shipping,);
+        CardPile[i++] = new Move("Ryk frem til Strandvejen. Hvis De passere START, indkasser da 4000kr."i++, 39);
+        CardPile[i++] = new Move("Tag til Rådhuspladsen",i++,40);
+        CardPile[i++] = new JailCard("I anledning af kongens fødselsdag benådes De herved for fængsel. Dette kort kan opbevares indtil De får brug for det, eller det kan sælges",i++,14);
+        CardPile[i++] = new JailCard("I anledning af kongens fødselsdag benådes De herved for fængsel. Dette kort kan opbevares indtil De får brug for det, eller det kan sælges",i++,14);
+        CardPile[i++] = new JailCard("Gå i fængse, De indkasserer ikke 4000kr for at passere start.", i++,14);
+        CardPile[i++] = new JailCard("Gå i fængse, De indkasserer ikke 4000kr for at passere start.", i++,14);
+    }
 
 
 
