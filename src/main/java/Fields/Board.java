55d5d4a364d2ff61fb46b5b6c4a9bdef249c1079
fiedler12 <o.lyngholm@gmail.com>
2021-01-06 22:26:02 +0100

    Field Array

--- src/main/java/Fields/Board.java
+++ src/main/java/Fields/Board.java
@@ -17,7 +17,7 @@ public class Board {
             board[i++] = new Street(i,1200, "Rødovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Chancefield(i);
             board[i++] = new Street(i,1200, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new TaxField(i);
+            board[i++] = new TaxField(i, false, 4000);
             board[i++] = new Shipping(4000,"Scandlines");
             board[i++] = new Street(i,2000, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Chancefield(i);
@@ -25,12 +25,12 @@ public class Board {
             board[i++] = new Street(i,2400, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Jail(i);
             board[i++] = new Street(i,2800, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new Brewery(3000);
+            board[i++] = new Brewery(i, 3000, 10);
             board[i++] = new Street(i,2800, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Street(i,3200, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Shipping(4000,"Mols");
             board[i++] = new Street(i,3600, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new Chancefield();
+            board[i++] = new Chancefield(i);
             board[i++] = new Street(i,3600, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Street(i,4000, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Parking(i);
@@ -38,10 +38,10 @@ public class Board {
             board[i++] = new Chancefield(i);
             board[i++] = new Street(i,4400, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Street(i,4800, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new Shipping(,"Scandlines2");
+            board[i++] = new Shipping(4000,"Scandlines2");
             board[i++] = new Street(i,5200, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new Street(i,5200, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new Brewery(3000);
+            board[i++] = new Brewery(i, 3000, 10);
             board[i++] = new Street(i,5800, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
             board[i++] = new GoToJail(i);
             board[i++] = new Street(i,6000, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
@@ -51,7 +51,7 @@ public class Board {
             board[i++] = new Shipping(4000,"Scandlines3");
             board[i++] = new Chancefield(i);
             board[i++] = new Street(i,7000, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
-            board[i++] = new TaxField(i);
+            board[i++] = new TaxField(i, true, 4000);
             board[i++] = new Street(i,8000, "Hvidovrevej", ".", ".", "", Color.BLUE, 0, 600, 1000, 250, 750, 2250, 4000, 6000);
     }
 
