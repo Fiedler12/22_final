c25eed368ee185cabf8f9d1a4a93d47d34982d30
UnoDosAndres <s205450@student.dtu.dk>
2021-01-07 17:18:08 +0100

    Tilføjet tekst og rettet stavefejl

--- src/main/java/com/company/Dice.java
+++ src/main/java/com/company/Dice.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class Dice {
-        int dye1;  //Første terning
-        int dye2;
+        int die1;  //Første terning
+        int die2;
 
 
         public Dice() {   //roll funktion
@@ -12,14 +12,14 @@ public class Dice {
         public void roll() {  /*Vi får roll funktionen til at sørge for at hver terning giver os et tilfældigt
         tal mellem 1-6 for hver gang vi roller. Det er også nemt at ændre værdien af terningen
         og det er også nemt at sørge for at hver terning har et vidst antal øjne*/
-            dye1 = (int) (Math.random() * 6 + 1);
-            dye2 = (int) (Math.random() * 6 + 1);
+            die1 = (int) (Math.random() * 6 + 1);
+            die2 = (int) (Math.random() * 6 + 1);
 
         }
 
         public int getTotal() { //Giv et resultat for de 2 terninger lagt sammen
             int total;
-            total = dye1 + dye2;
+            total = die1 + die2;
             return total;
         }
 }
