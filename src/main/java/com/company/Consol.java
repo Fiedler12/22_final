76a3965214c38d337f59ab6140d3fc744929307d
fiedler12 <o.lyngholm@gmail.com>
2021-01-10 13:43:49 +0100

    Merging af chancecard og tur

--- src/main/java/com/company/Consol.java
+++ src/main/java/com/company/Consol.java
@@ -125,9 +125,14 @@ public class Consol {
     public void playerRolls(int playerIndex) {
         dice.roll();
         gui.setDice(dice.die1, dice.die2);
+        gui.getUserButtonPressed("Du har slået:" + dice.getTotal() + ", tryk har for at rykke", "Ryk");
         gui.getFields()[PlayerController.players[playerIndex].getPos()].setCar(playerController.getGui_players()[playerIndex], false);
         playerController.movePlayer(playerIndex, dice.getTotal());
         gui.getFields()[PlayerController.players[playerIndex].getPos()].setCar(playerController.getGui_players()[playerIndex], true);
+        boolean checkChance = (boardController.getField()[PlayerController.players[playerIndex].getPos()] instanceof Chancefield);
+        if (checkChance) {
+            pullCard(playerIndex);
+        }
         boolean checkSubClass = (boardController.getField()[PlayerController.players[playerIndex].getPos()] instanceof Ownable);
         if (checkSubClass) {
             Ownable ownable = (Ownable) boardController.getField()[PlayerController.players[playerIndex].getPos()];
